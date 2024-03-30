f49bee39767936260f6693bf8b7223ec8a28b925
Ani <bloodraven166@gmail.com>
2024-03-29 18:15:04 -0700

    Trigger my updated workflow and check/test output (dividing the atime step runtime into installation and test time)

--- inst/atime/tests.R
+++ inst/atime/tests.R
@@ -83,3 +83,4 @@ test.list <- list(
     "Regression" = "6f360be0b2a6cf425f6df751ca9a99ec5d35ed93",
     "Fixed" = "ba32f3cba38ec270587e395f6e6c26a80be36be6")   
 )
+# Just a change to trigger my updated workflow and check/test output
