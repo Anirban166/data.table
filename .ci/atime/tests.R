5467fd33f528cdc098e38f0cb98f7166fb18930a
Ani <bloodraven166@gmail.com>
2024-04-18 13:00:38 -0700

    Removed unnecessary double quotes

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -119,9 +119,9 @@ test.list <- list(
   #    v1 = sample(5L, N, TRUE),
   #    v2 = sample(5L, N, TRUE))
   #  }),
-  #  "Before" = "15f0598b9828d3af2eb8ddc9b38e0356f42afe4f",
-  #  "Regression" = "c0052964694a4c618ab182aa474f924d40576d94", # Since regression was reported on Jan 26 2020 (https://github.com/Rdatatable/data.table/issues/4200)
-  #  "Fixed" = "ba32f3cba38ec270587e395f6e6c26a80be36be6"), # Merge commit in the PR that fixed the regression (https://github.com/Rdatatable/data.table/pull/4558)
+  #  Before = "15f0598b9828d3af2eb8ddc9b38e0356f42afe4f",
+  #  Regression = "c0052964694a4c618ab182aa474f924d40576d94", # Since regression was reported on Jan 26 2020 (https://github.com/Rdatatable/data.table/issues/4200)
+  #  Fixed = "ba32f3cba38ec270587e395f6e6c26a80be36be6"), # Merge commit in the PR that fixed the regression (https://github.com/Rdatatable/data.table/pull/4558)
 
   # Issue reported in: https://github.com/Rdatatable/data.table/issues/5426
   # To be fixed in: https://github.com/Rdatatable/data.table/pull/5427
@@ -132,6 +132,6 @@ test.list <- list(
     DT = replicate(N, 1, simplify = FALSE)
   }),
   expr = quote(data.table:::setDT(DT)),
-  "Slow" = "c4a2085e35689a108d67dacb2f8261e4964d7e12", # Parent of the first commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/commit/7cc4da4c1c8e568f655ab5167922dcdb75953801)
-  "Fast" = "1872f473b20fdcddc5c1b35d79fe9229cd9a1d15") # Last commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/pull/5427/commits)  
+  Slow = "c4a2085e35689a108d67dacb2f8261e4964d7e12", # Parent of the first commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/commit/7cc4da4c1c8e568f655ab5167922dcdb75953801)
+  Fast = "1872f473b20fdcddc5c1b35d79fe9229cd9a1d15") # Last commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/pull/5427/commits)  
 )
