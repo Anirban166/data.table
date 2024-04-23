d0e2a97cb9cee137b8df6beedf06d58b17d75b23
Ani <bloodraven166@gmail.com>
2024-04-23 13:47:10 -0700

    Update tests.R

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -132,6 +132,9 @@ test.list <- list(
     L <- replicate(N, 1, simplify = FALSE)
     setDT(L)    
   }),
-  expr = quote(data.table:::setattr(L, "class", NULL); data.table:::setDT(DT)),
+  expr = quote({
+    data.table:::setattr(L, "class", NULL) 
+    data.table:::setDT(L)
+  }),
   Slow = "c4a2085e35689a108d67dacb2f8261e4964d7e12", # Parent of the first commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/commit/7cc4da4c1c8e568f655ab5167922dcdb75953801)
   Fast = "1872f473b20fdcddc5c1b35d79fe9229cd9a1d15") # Last commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/pull/5427/commits)  
