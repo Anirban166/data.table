e02c93516fc8aa42373ce1f6d9dfe14610ea7fe1
Ani <bloodraven166@gmail.com>
2024-04-29 14:39:31 -0700

    Update tests.R

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -1,27 +1,25 @@
-# A function to customize R package metadata and source files to facilitate version-specific installation and testing.
+# A list of performance tests.
 #
-# This is specifically tailored for handling data.table which requires specific changes in non-standard files (such as the object file name in Makevars and version checking code in onLoad.R)
-# to support testing across different versions (base and HEAD for PRs, commits associated with historical regressions, etc.) of the package.
-# It appends a SHA1 hash to the package name (PKG.SHA), ensuring each version can be installed and tested separately.
+# Each entry in this list corresponds to a performance test and contains a sublist with three mandatory arguments:
+# - N: A numeric sequence of data sizes to vary.
+# - setup: An expression evaluated for every data size before measuring time/memory.
+# - expr: An expression that will be evaluated for benchmarking performance across different git commit versions.
+#         This must call a function from data.table using a syntax with double or triple colon prefix.
+#         The package name before the colons will be replaced by a new package name that uses the commit SHA hash.
+#         (For instance, data.table:::[.data.table will become data.table.some_40_digit_SHA1_hash:::[.data.table)
 #
-# @param old.Package Current name of the package.
-# @param new.Package New name of the package, including a SHA hash.
-# @param sha SHA1 hash used for differentiating versions.
-# @param new.pkg.path Path to the package files.
-#
-# @details
-# The function modifies:
-# - DESCRIPTION, updating the package name.
-# - Makevars, customizing the shared object file name and adjusting the build settings.
-# - R/onLoad.R, adapting custom version checking for package loading operations.
-# - NAMESPACE, changing namespace settings for dynamic linking.
-#
-# @examples
-# pkg.edit.fun("data.table", "data.table.some_SHA1_hash", "some_SHA1_hash", "/path/to/data.table")
-#
-# @return None (performs in-place file modifications)
-# @note This setup is typically unnecessary for most packages but essential for data.table due to its unique configuration.
-pkg.edit.fun = function(old.Package, new.Package, sha, new.pkg.path) {
+# Optional parameters that may be useful to configure tests:
+# - times: Number of times each expression is evaluated (default is 10).
+# - seconds.limit: The maximum median timing (in seconds) of an expression. No timings for larger N are computed past that threshold. Default of 0.01 seconds should be sufficient for most tests.
+# - Historical references (short version name = commit SHA1).
+#   For historical regressions, use version names 'Before', 'Regression', and 'Fixed'
+#   When there was no regression, use 'Slow' and 'Fast' 
+# @note Please check https://github.com/tdhock/atime/blob/main/vignettes/data.table.Rmd for more information.
+# nolint start: undesirable_operator_linter. ':::' needed+appropriate here.
+test.list <- atime::atime_test_list(
+  # Performance regression discussed in: https://github.com/Rdatatable/data.table/issues/4311
+  # Fixed in: https://github.com/Rdatatable/data.table/pull/4440
+  pkg.edit.fun = function(old.Package, new.Package, sha, new.pkg.path) {
       pkg_find_replace <- function(glob, FIND, REPLACE) {
         atime::glob_find_replace(file.path(new.pkg.path, glob), FIND, REPLACE)
       }
@@ -52,31 +50,9 @@ pkg.edit.fun = function(old.Package, new.Package, sha, new.pkg.path) {
         "NAMESPACE",
         sprintf('useDynLib\\("?%s"?', Package_regex),
         paste0('useDynLib(', new.Package_))
-    }
-
-# A list of performance tests.
-#
-# Each entry in this list corresponds to a performance test and contains a sublist with three mandatory arguments:
-# - N: A numeric sequence of data sizes to vary.
-# - setup: An expression evaluated for every data size before measuring time/memory.
-# - expr: An expression that will be evaluated for benchmarking performance across different git commit versions.
-#         This must call a function from data.table using a syntax with double or triple colon prefix.
-#         The package name before the colons will be replaced by a new package name that uses the commit SHA hash.
-#         (For instance, data.table:::[.data.table will become data.table.some_40_digit_SHA1_hash:::[.data.table)
-#
-# Optional parameters that may be useful to configure tests:
-# - times: Number of times each expression is evaluated (default is 10).
-# - seconds.limit: The maximum median timing (in seconds) of an expression. No timings for larger N are computed past that threshold. Default of 0.01 seconds should be sufficient for most tests.
-# - Historical references (short version name = commit SHA1).
-#   For historical regressions, use version names 'Before', 'Regression', and 'Fixed'
-#   When there was no regression, use 'Slow' and 'Fast' 
-# @note Please check https://github.com/tdhock/atime/blob/main/vignettes/data.table.Rmd for more information.
-# nolint start: undesirable_operator_linter. ':::' needed+appropriate here.
-test.list <- list(
-  # Performance regression discussed in: https://github.com/Rdatatable/data.table/issues/4311
-  # Fixed in: https://github.com/Rdatatable/data.table/pull/4440
+    },    
   "shallow regression fixed in #4440" = atime::atime_test(
-    pkg.edit.fun = pkg.edit.fun,    
+    pkg.edit.fun = pkg.edit.fun,
     N = 10^seq(3,8),
     setup = {
       set.seed(1L)
@@ -92,7 +68,7 @@ test.list <- list(
   # Performance regression introduced from a commit in: https://github.com/Rdatatable/data.table/pull/4491
   # Fixed in: https://github.com/Rdatatable/data.table/pull/5463
   "memrecycle regression fixed in #5463" = atime::atime_test(
-    pkg.edit.fun = pkg.edit.fun,    
+    pkg.edit.fun = pkg.edit.fun,
     N = 10^seq(3, 8),
     setup = {
       n <- N/100
@@ -111,7 +87,7 @@ test.list <- list(
   # Issue reported in: https://github.com/Rdatatable/data.table/issues/5426
   # To be fixed in: https://github.com/Rdatatable/data.table/pull/5427
   "setDT improved in #5427" = atime::atime_test(
-    pkg.edit.fun = pkg.edit.fun,    
+    pkg.edit.fun = pkg.edit.fun,
     N = 10^seq(1, 7),
     setup = {
       L <- replicate(N, 1, simplify = FALSE)
@@ -124,4 +100,3 @@ test.list <- list(
     Slow = "c4a2085e35689a108d67dacb2f8261e4964d7e12", # Parent of the first commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/commit/7cc4da4c1c8e568f655ab5167922dcdb75953801)
     Fast = "1872f473b20fdcddc5c1b35d79fe9229cd9a1d15") # Last commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/pull/5427/commits)
 )
-# nolint end: undesirable_operator_linter.
