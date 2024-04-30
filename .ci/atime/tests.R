dadba67f2994296292d0dbc0e6f9e7aae3ade6f1
Ani <bloodraven166@gmail.com>
2024-04-29 18:39:33 -0700

    Use the merge commit

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -121,6 +121,6 @@ test.list <- list(
     data.table:::setDT(L)
   }),
   Slow = "c4a2085e35689a108d67dacb2f8261e4964d7e12", # Parent of the first commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/commit/7cc4da4c1c8e568f655ab5167922dcdb75953801)
-  Fast = "1872f473b20fdcddc5c1b35d79fe9229cd9a1d15") # Last commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/pull/5427/commits)
+  Fast = "2487c61656335764980e478c323f7e6ce4e6d4ca") # Merge commit in the PR that fixes the issue (https://github.com/Rdatatable/data.table/pull/5427)
 )
 # nolint end: undesirable_operator_linter.
