62302d2788ed91926e3c41087735e5b3e48ae492
Ani <bloodraven166@gmail.com>
2024-07-19 12:01:20 -0700

    Update tests.R

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -82,7 +82,7 @@ test.list <- atime::atime_test_list(
       dt <- as.data.table(df)
     },
     expr = data.table:::`[.data.table`(transform(dt, y = round(x))),
-    Slow = "0895fa247afcf6b38044bd5f56c0d209691ddb31",,
+    Slow = "bf499090c0e6fd5cb492bf8b1603d93c1ee21dfb",
     Fast = "2d1a0575f87cc50e90f64825c30d7a6cb6b05dd7")
 )
 # nolint end: undesirable_operator_linter.
