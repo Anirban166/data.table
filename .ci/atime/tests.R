e09d6f301ec5f1af5aee7931a4c41623c583c8d4
Ani <bloodraven166@gmail.com>
2024-07-30 17:46:23 -0700

    Switching to perf regression labels and adding a comment on the process kill for N = 1e9

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -75,7 +75,7 @@ test.list <- atime::atime_test_list(
 
   # Fixed in: https://github.com/Rdatatable/data.table/pull/4386
   "forder improved in #4386" = atime::atime_test(
-    N = 10^seq(1, 8),
+    N = 10^seq(1, 8), # 1e9 probably exceeds runner's memory (gets killed)
     setup = {
       dt <- data.table(id = sample(N), value = rnorm(N))
     },
@@ -84,9 +84,10 @@ test.list <- atime::atime_test_list(
       dt[order(data.table:::forder(dt, "id"))]
       dt[order(data.table:::forder(dt, "id"))]
       # dt[order(data.table:::forder(dt, "id"))]
-    },  
-    Slow = "c152ced0e5799acee1589910c69c1a2c6586b95d", # Parent of the merge commit that fixes the regression
-    Fast = "1a84514f6d20ff1f9cc614ea9b92ccdee5541506"), # Merge commit of the PR (https://github.com/Rdatatable/data.table/pull/4386/commits) that fixes the regression
+    },
+    Before = "aa75d79376478b3e8f80fd6f31dcf53be8bf3404" # 1.15.4
+    Regression = "c152ced0e5799acee1589910c69c1a2c6586b95d", # Parent of the merge commit that fixes the regression
+    Fixed = "1a84514f6d20ff1f9cc614ea9b92ccdee5541506"), # Merge commit of the PR (https://github.com/Rdatatable/data.table/pull/4386/commits) that fixes the regression
 NULL
 )
 # nolint end: undesirable_operator_linter.
