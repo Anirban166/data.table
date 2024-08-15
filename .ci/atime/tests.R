df592a4a0840c6b3f803c25df32087fc4407813d
Ani <bloodraven166@gmail.com>
2024-08-14 18:10:33 -0700

    Testing something.

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -77,13 +77,15 @@ test.list <- atime::atime_test_list(
   "forder improved in #4386" = atime::atime_test(
     N = 10^seq(1, 8), # 1e9 probably exceeds the runner's memory (process gets killed)
     setup = {
-      dt <- data.table(id = sample(N), value = rnorm(N))
+      dt <- data.table(a = sample(letters, N, TRUE), b = rnorm(N))
+      setkey(dt, a)
     },
     expr = {
-      data.table:::setkey(dt, id)
-      dt[order(data.table:::forder(dt, "id"))]
-      dt[order(data.table:::forder(dt, "id"))]
-      # dt[order(data.table:::forder(dt, "id"))]
+      # data.table:::setkey(dt, id)
+      options(datatable.forder.reuse.sorting=TRUE, datatable.verbose=TRUE)
+      dt[order(data.table:::forder(dt, "a"))]
+      dt[order(data.table:::forder(dt, "a"))]
+      # dt[order(data.table:::forder(dt, "a"))]
     },
     Slow = "c152ced0e5799acee1589910c69c1a2c6586b95d", # Parent of the merge commit that fixes the regression
     Fast = "1a84514f6d20ff1f9cc614ea9b92ccdee5541506"), # Merge commit of the PR (https://github.com/Rdatatable/data.table/pull/4386/commits) that fixes the regression
