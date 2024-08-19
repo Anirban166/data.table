6fef7242c93882714711dcc7a021b349436bed92
Ani <bloodraven166@gmail.com>
2024-08-19 13:20:34 -0700

    Update tests.R

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -100,7 +100,7 @@ test.list <- atime::atime_test_list(
       # dt[order(data.table:::forder(dt, c("a", "d")))]
       # dt[order(data.table:::forder(dt, c("b", "d")))]
       dt[order(data.table:::forder(dt, c("a", "b")))]
-      dt[order(data.table:::forder(dt, c("b", "c")))]
+      # dt[order(data.table:::forder(dt, c("b", "c")))]
     },
     Slow = "c152ced0e5799acee1589910c69c1a2c6586b95d", # Parent of the merge commit that fixes the regression
     Fast = "1a84514f6d20ff1f9cc614ea9b92ccdee5541506"), # Merge commit of the PR (https://github.com/Rdatatable/data.table/pull/4386/commits) that fixes the regression
